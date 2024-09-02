fbeb4532cf027f97ec84bf98d9c846e8ff003bb9
hyesungKomet <khsfun0312@naver.com>
2024-09-02 17:15:47 +0900

    change TCN layer

--- model/multi_input_tcn_resnet50.py
+++ model/multi_input_tcn_resnet50.py
@@ -44,8 +44,11 @@ class MultilevelTCNModel(nn.Module):
 
         self.global_avg_pool = nn.AdaptiveAvgPool1d(1)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(512 * 6, 128)  # Combine the output from all three TCN blocks
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(512 * 6, 256)  # Combine the output from all three TCN blocks
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 32)
+        self.fc4 = nn.Linear(32, num_classes)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, chroma, mfcc, mel):
         # Extract features using VGG19
@@ -81,9 +84,9 @@ class MultilevelTCNModel(nn.Module):
             mfcc_features2 = block(mfcc_features2)
 
         # Concatenate the outputs from both TCN blocks
-        mel_features = torch.cat((mel_features1, mel_features2), dim=1)
-        mfcc_features = torch.cat((mfcc_features1, mfcc_features2), dim=1)
-        chroma_features = torch.cat((chroma_features1, chroma_features2), dim=1)
+        mel_features = torch.cat((mel_features1, mel_features2), dim=2)
+        mfcc_features = torch.cat((mfcc_features1, mfcc_features2), dim=2)
+        chroma_features = torch.cat((chroma_features1, chroma_features2), dim=2)
 
         # Global average pooling
         mel_out = self.global_avg_pool(mel_features).squeeze(-1)
@@ -95,7 +98,10 @@ class MultilevelTCNModel(nn.Module):
         combined_out = self.flatten(combined_out)
 
         # Fully connected layers
-        combined_out = self.fc1(combined_out)
-        combined_out = self.fc2(combined_out)
+        x = self.fc1(combined_out)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        x = self.fc4(x)
+        output = self.softmax(x)
 
-        return combined_out
+        return output
